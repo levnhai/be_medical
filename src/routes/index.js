95fd9fcb6caef8918d81ee826a89b7bc2a9e4caa
LAPTOP-9JMLLIVP\DAT <fbui9506@gmail.com>
2024-09-10 10:26:56 +0700

    hello

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const homeRouter = require('./home');
+const newsRouter = require('./news');
+const categorynewsRouter = require('./categorynews');
+
 function route(app) {
   app.use('/', homeRouter);
+  app.use('/', newsRouter);
+  app.use('/', categorynewsRouter);
 }
-
 module.exports = route;
